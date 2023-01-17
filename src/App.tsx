2bb4ff6fd37081a4eda519379beb69d3695c3773
kjw8319 <kjw8319@sjmnc.kr>
2023-01-17 12:25:14 +0900

    Feat ) Gnb 생성 1. Gnb 생성 2. 장바구니, 결제 페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { useRoutes } from 'react-router-dom';
+import Gnb from './components/Gnb';
 import { getClient } from './pages/queryClient';
 import { routes } from './routes';
 
@@ -10,6 +11,7 @@ const App = () => {
 
   return (
     <QueryClientProvider client={queryClient}>
+      <Gnb />
       {element}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,8 @@ import GlobalLayout from './pages/_layout';
 import Index from './pages/index';
 import ProductsIndex from './pages/products/index';
 import ProductsId from './pages/products/[id]';
+import Cart from './pages/cart';
+import Payment from './pages/payment';
 
 export const routes = [
   {
@@ -12,6 +14,8 @@ export const routes = [
       { path: '/', element: <Index />, index: true },
       { path: '/products', element: <ProductsIndex />, index: true },
       { path: '/products/:id', element: <ProductsId /> },
+      { path: '/cart', element: <Cart />, index: true },
+      { path: '/payment', element: <Payment />, index: true },
     ],
   },
 ];
