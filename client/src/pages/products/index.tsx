3e56eef2fd325e856e949b8835680662c99a3c60
kjw-blog <rkdwjddnr11@naver.com>
2023-03-29 11:36:02 +0900

    Feat ) 상품목록 fetching 함수 useQuery => useInfiniteQuery로 변경

--- client/src/pages/products/index.tsx
+++ client/src/pages/products/index.tsx
@@ -1,18 +1,44 @@
-import { useQuery } from 'react-query';
+import { useInfiniteQuery, useQuery } from 'react-query';
 import ProductItem from '../../components/product/item';
 import ProductList from '../../components/product/list';
 import GET_PRODUCTS, { Products } from '../../graphql/products';
 import { graphqlFetcher, QueryKeys } from '../queryClient';
 
 const ProductListPage = () => {
-  const { data } = useQuery<Products>(QueryKeys.PRODUCTS, () =>
-    graphqlFetcher(GET_PRODUCTS)
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery<Products>(
+    QueryKeys.PRODUCTS,
+    ({ pageParam = '' }) => graphqlFetcher(GET_PRODUCTS, { cursor: pageParam }),
+    {
+      // 이 함수의 return 값이 pageParam으로 들어가게된다.
+      getNextPageParam: (lastPage, allPages) => {
+        return lastPage.products.at(-1)?.id;
+      },
+    }
   );
+
+  /**
+   * infiniteQuery가 내려주는 data 형식
+   *
+   * data : {
+   *  pages: [
+   *    { products: [ ... ] },
+   *    { products: [ ... ] } ,
+   *    { products: [ ... ] }
+   *  ],
+   *  pageParams: [undefined, ... ]
+   * }
+   *
+   * 데이터 표출 방법
+   * 1. map 안에서 map 을 한번 더 돌린다.
+   *   (강사님 권장방법, 페이지 별 데이터 식별이 쉬움)
+   * 2. data.pages.flatMap(page => page.products)를 사용해서 page안에 products을
+   *    추출해서 새로운 배열을 만든다.
+   */
 
   return (
     <div>
       <h2>상품 목록</h2>
-      <ProductList list={data?.products || []} />
+      <ProductList list={data?.pages || []} />
     </div>
   );
 };
